ce0862cd042596f695f271a50de2bf9adaf35ac7
AryaDuhan <aryaduhan28@gmail.com>
2025-07-11 03:44:49 +0530

    yes

--- configure/script.js
+++ configure/script.js
@@ -4,7 +4,7 @@ const urlParams = new URLSearchParams(queryString);
 const code = urlParams.get("code") || "";
 
 const baseURL = "https://aryaduhan.github.io/spotify-widget";
-const redirect_uri = `${baseURL}/configure`;
+const redirect_uri = `${baseURL}/configure/`;
 let refresh_token = "";
 let access_token = "";
 let browserSourceURL = "";
