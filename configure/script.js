67dfc82ca2aaec8d6ba156f2d4ce6c1a5f42ba04
AryaDuhan <aryaduhan28@gmail.com>
2025-07-11 03:28:46 +0530

    i will be mad if this worked

--- configure/script.js
+++ configure/script.js
@@ -3,7 +3,7 @@ const urlParams = new URLSearchParams(queryString);
 
 const code = urlParams.get("code") || "";
 
-const baseURL = "https://AryaDuhan.github.io/spotify-widget";
+const baseURL = "https://aryaduhan.github.io/spotify-widget";
 const redirect_uri = `${baseURL}/configure`;
 let refresh_token = "";
 let access_token = "";
